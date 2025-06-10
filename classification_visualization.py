67e8414d47739433479ede1a8fe9a7307b14c283
JMocklerUMD <jmockle1@umd.edu>
2025-06-10 14:59:53 -0400

    Pushing all new changes to Joe development branch
    Includes updates to visualization and training

---
+++ classification_visualization.py
@@ -0,0 +1,285 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+import numpy as np
+import os
+
+import keras
+import tensorflow as tf
+
+
+from keras.applications import resnet50
+from keras.models import Model
+
+from keras.preprocessing import image
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import array_to_img
+
+
+
+#%% Function Calls + Resnet50 instantiation
+# This function tells our feature extractor to do its thing
+def get_bottleneck_features(model, input_imgs):
+	print('Getting Feature Data From ResNet...')
+	features = model.predict(input_imgs, verbose = 1)
+	return features
+
+def img_preprocess(input_image):
+    input_image = np.stack((input_image,)*3,axis = -1)
+    input_image = array_to_img(input_image)
+    input_image = input_image.resize((224,224))
+    input_image = img_to_array(input_image)
+    #input_image = (input_image / 127.5) - 1
+    return input_image
+
+def get_bottleneck_features(model, input_imgs):
+	print('Getting Feature Data From ResNet...')
+	features = model.predict(input_imgs, verbose = 1)
+	return features
+
+model1 = resnet50.ResNet50(include_top = False, weights ='imagenet', input_shape = (224,224,3))
+output = model1.output
+output = tf.keras.layers.Flatten()(output)
+resnet_model = Model(model1.input,output)
+
+# load the classifier
+model = keras.models.load_model('C:\\Users\\Joseph Mockler\\Documents\\GitHub\\2025_Hypersonic_BL_ID\\trained_classifier_RandomSampled.keras')
+
+
+#%% read in images
+print('Reading training data file')
+
+# Write File Name
+file_name = 'C:\\UMD GRADUATE\\RESEARCH\\Hypersonic Image ID\\training_data_explicit.txt'
+if os.path.exists(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+else:
+    raise ValueError("No training_data file detected")
+
+lines_len = len(lines)
+print(f"{lines_len} lines read")
+
+
+#%% Split the image into 20 pieces
+def image_splitting(i, lines):
+    WP_io = []
+    #SM_bounds_Array = []
+    Imagelist = []
+    
+    curr_line = i;
+    line = lines[curr_line]
+    
+    parts = line.strip().split()
+    
+    run = parts[0]
+    image_response = parts[1]
+    sm_check = parts[2]
+    if sm_check.startswith('X'):
+    	sm_bounds = list(map(str, parts[2:6]))  # Convert bounds to integers
+    else:
+    	sm_bounds = list(map(int, parts[2:6]))
+    image_size = list(map(int, parts[6:8]))  # Convert image size to integers
+    image_data = list(map(float, parts[8:]))  # Convert image data to floats
+    
+    # Reshape the image data into the specified image size
+    full_image = np.array(image_data).astype(np.float64)
+    full_image = full_image.reshape(image_size)  # Reshape to (rows, columns)
+    
+    if full_image.shape != (64, 1280):
+        print(f"Skipping image at line {i+1} — unexpected size {full_image.shape}")
+        #continue
+    
+    slice_width = 64
+    height, width = full_image.shape
+    num_slices = width // slice_width
+    
+    # Only convert bounds to int if not sm_check.startswith('X')
+    if not sm_check.startswith('X'):
+        sm_bounds = list(map(int, sm_bounds))
+        x_min, y_min, box_width, box_height = sm_bounds
+        x_max = x_min + box_width
+        y_max = y_min + box_height
+    
+    for i in range(num_slices):
+        x_start = i * slice_width
+        x_end = (i + 1) * slice_width
+    
+        # Slice the image
+        image = full_image[:, x_start:x_end]
+        image_size = image.shape
+        Imagelist.append(image)
+    
+        if sm_check.startswith('X'):
+            WP_io.append(0)
+    
+        else:
+            # Check for horizontal overlap with this slice
+            if x_max >= x_start and x_min <= x_end:
+                WP_io.append(1)
+    
+            else:
+                WP_io.append(0)
+                
+    return Imagelist, WP_io, slice_width, height, sm_bounds
+
+def classify_the_images(model, Imagelist):
+    Imagelist_resized = np.array([img_preprocess(img) for img in Imagelist])
+    
+    # Run through feature extractor
+    Imagelist_res = get_bottleneck_features(resnet_model, Imagelist_resized)
+    
+    # Pass each through the trained NN
+    test_res= model.predict(Imagelist_res)
+    classification_result = np.round(test_res)
+    
+    return classification_result
+
+
+#%% Iterate through the list!
+N_img = 1000
+acc_history = []
+TP_history = []
+TN_history = []
+FP_history = []
+FN_history = []
+plot_flag = 1       # View the images? MUCH SLOWER
+
+for i_iter in range(N_img):
+    
+    Imagelist, WP_io, slice_width, height, sm_bounds = image_splitting(i_iter, lines)
+    
+    classification_result = classify_the_images(model, Imagelist)
+  
+    # Restack and plot the image
+    imageReconstruct = np.hstack([image for image in Imagelist])
+    
+    if plot_flag == 1:
+        fig, ax = plt.subplots(1)
+        ax.imshow(imageReconstruct, cmap = 'gray')
+    
+    # build out the components of a confusion matrix
+    n00, n01, n10, n11 = 0, 0, 0, 0 
+               
+    # Add on classification box rectangles
+    for i, _ in enumerate(Imagelist):
+        
+        # Get stats on the current image
+        if WP_io[i] == 0:
+            if classification_result[i] == 0:
+                n00 += 1
+            if classification_result[i] == 1:
+                n01 += 1 
+        elif WP_io[i] == 1:
+            if classification_result[i] == 0:
+                n10 += 1
+            if classification_result[i] == 1:
+                n11 += 1
+        
+        # Add in the classification guess
+        if classification_result[i] == 1:
+            rect = Rectangle((i*slice_width, 5), slice_width, height-10,
+                                     linewidth=0.5, edgecolor='red', facecolor='none')
+            ax.add_patch(rect)
+    
+    # Compute the inter-image accuracy
+    acc = (n00 + n11) / (n00 + n11 + n10 + n01)
+    print(f'Image {i_iter}: accuracy = {acc}')
+    
+    # Save off data for whole-set analysis
+    TP_history.append(n11)
+    TN_history.append(n00)
+    FP_history.append(n01)
+    FN_history.append(n10)
+    acc_history.append(acc)
+    
+    if plot_flag == 1:
+        # Check if there's even a bounding box in the image
+        if sm_bounds[0] == 'X':
+            ax.set_title('Image '+str(i_iter)+'. Blue: true WP. Red: NN class')
+            plt.show()
+            continue
+        else:
+            # Add the ground truth over the entire box
+            ax.add_patch(Rectangle((sm_bounds[0],sm_bounds[1]), sm_bounds[2], sm_bounds[3], edgecolor='blue', facecolor='none'))
+        
+            ax.set_title('Image '+str(i_iter)+'. Blue: true WP. Red: NN class')
+            plt.show()
+
+#%% Make history plot
+
+# Take a rolling average
+def moving_average(a, n):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+n = 20
+fig, (pl1, pl2, pl3, pl4, pl5) = plt.subplots(5,1, figsize = (16,16))
+pl1.plot(range(len(acc_history)), acc_history)
+pl1.plot(range(n-1, len(acc_history)), moving_average(acc_history, n), color='k', linewidth = 2)
+pl1.set_title('Accuracy')
+
+pl2.plot(range(len(TP_history)), TP_history)
+pl2.plot(range(n-1, len(acc_history)), moving_average(TP_history, n), color='k', linewidth = 2)
+pl2.set_title('True positive rate')
+
+pl3.plot(range(len(TN_history)), TN_history)
+pl3.plot(range(n-1, len(TN_history)), moving_average(TN_history, n), color='k', linewidth = 2)
+pl3.set_title('True negative rate')
+
+pl4.plot(range(len(FP_history)), FP_history)
+pl4.plot(range(n-1, len(FP_history)), moving_average(FP_history, n), color='k', linewidth = 2)
+pl4.set_title('False positive rate')
+
+pl5.plot(range(len(FN_history)), FN_history)
+pl5.plot(range(n-1, len(FN_history)), moving_average(FN_history, n), color='k', linewidth = 2)
+pl5.set_title('False negative rate')
+
+
+#%% Compute MICE
+MICE = []
+for i in range(len(acc_history)):
+    n0 = TP_history[i] + FP_history[i]
+    n1 = TN_history[i] + FN_history[i]
+    
+    A0 = (n0/20)**2 + (n1/20)**2
+    if np.isclose(1-A0, 0):
+        MICE.append(0.0)
+    else:
+        MICE.append((acc_history[i] - A0)/(1-A0))
+    
+    
+fig, ax = plt.subplots(1,1, figsize = (16,6))
+ax.plot(range(len(MICE)), MICE)
+ax.plot(range(n-1, len(MICE)), moving_average(MICE, n), color='k', linewidth = 2)
+ax.set_ylim(-1,1)
+ax.set_title('MICE Performance')
+
+
+#%% Print out the entire data set statistics
+print("Data set statistics")
+print("----------------------------------------")
+print(f"Whole-set Average: {np.mean(acc_history)}")
+print(f"Whole-set True Positive rate: {np.mean(TP_history)/20}")
+print(f"Whole-set True Negative rate: {np.mean(TN_history)/20}")
+print(f"Whole-set False Positive rate: {np.mean(FP_history)/20}")
+print(f"Whole-set False Negative rate: {np.mean(FN_history)/20}")
+print(f"Whole-set MICE Score: {np.mean(MICE)}")
+
+
+#%% misc tests
+# Restack and plot the image
+#imageReconstruct = np.hstack([image for image in Imagelist])
+
+#fig, ax = plt.subplots(1)
+#ax.imshow(imageReconstruct, cmap = 'gray')
+
+#Imagelist, WP_io, slice_width, height, sm_bounds = image_splitting(i_iter, lines)
+#ax.add_patch(Rectangle((sm_bounds[0],sm_bounds[1]), sm_bounds[2], sm_bounds[3], edgecolor='red', facecolor='none'))
+
+#plt.show()
+
+
+
